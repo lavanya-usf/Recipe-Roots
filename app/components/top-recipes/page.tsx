8924ccf6f3f202774501868601b63146d68d9c3c
Lavanya Chilakalapudi <lavanya13@usf.edu>
2025-03-17 22:03:24 -0400

    Added top navbar and search input

---
+++ app/components/top-recipes/page.tsx
@@ -0,0 +1,7 @@
+export default function Page() {
+    return (
+        <div>
+            <p>Top recipes will be displayed here......coming sooon</p>
+        </div>
+    )
+}
---
+++ tailwind.config.ts
@@ -0,0 +1,13 @@
+import 'tailwindcss';
+
+module.exports = {
+    theme: {
+      colors: {
+        purple: {
+            light: "#977FCB",
+            medium: "#5E4593",
+            dark: "#4E308F",
+        }
+      }
+    }
+  }
