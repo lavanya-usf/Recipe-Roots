20267f8c663ef1f5e3db7fa3001820d47241d17f
prataplakku <prataplakku@gmail.com>
2025-03-19 21:59:26 -0400

    Made some modificcations to header and added basic footer

--- components/ui/footer.tsx
+++ components/ui/footer.tsx
@@ -1,14 +1,14 @@
 import { Copyright } from 'lucide-react';
 export default function Footer(){
     return(
-        <div className="relative flex h-10 w-full items-center justify-center space-x-4 bg-gradient-to-r from-blue-900 to-blue-700 px-6 shadow-md text-white z-50">
-            <span>About Us</span>
-            <div className='flex flex-row space-x-2 items-center'> 
+        <div className="relative flex h-10 w-full items-center justify-center space-x-4 bg-gradient-to-r from-blue-900 to-blue-700 px-6 shadow-md text-white z-50 font-medium">
+            <span className=' hover:text-gray-400 transition duration-300 cursor-pointer'>About Us</span>
+            <div className='flex flex-row space-x-2 items-center  hover:text-gray-400 transition duration-300 cursor-pointer'> 
                 <Copyright size={18}/>
                 <span>Copyright</span>
             </div>
             
-            <span> Contact Us</span>
+            <span className=' hover:text-gray-400 transition duration-300 cursor-pointer'> Contact Us</span>
         </div>
     )
 
--- components/ui/navbarelement.tsx
+++ components/ui/navbarelement.tsx
@@ -7,7 +7,7 @@ interface NavbarElementProps {
 
 export const NavbarElement: React.FC<NavbarElementProps> = ({Icon, title}) => {
   return(
-    <div className="flex space-x-2 items-center text-white hover:text-gray-700 transition duration-300 cursor-pointer">
+    <div className="flex space-x-2 items-center text-white hover:text-gray-400 transition duration-300 cursor-pointer">
       <Icon size={22}/>
       <span className="text-lg font-medium">{title}</span>
     </div>
