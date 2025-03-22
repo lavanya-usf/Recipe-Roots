a448d14e6df86e3e804e3730a24657a7c699e0dc
prataplakku <prataplakku@gmail.com>
2025-03-22 00:06:57 -0400

    added banner

--- app/page.tsx
+++ app/page.tsx
@@ -2,11 +2,13 @@ import Navbar from "../components/ui/navbar";
 import TopPicks from '../components/top-recipes/page';
 import SearchBar from "../components/ui/searchbar";
 import Footer from "../components/ui/footer";
+import HeroBanner from "@/components/ui/heroBanner";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col p-2">
       <Navbar />
+      <HeroBanner />
       
       <div className="flex justify-between py-2">
         <SearchBar />
